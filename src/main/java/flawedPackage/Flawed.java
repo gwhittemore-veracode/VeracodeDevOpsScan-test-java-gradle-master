052eab2856f1ffad027c64f8e6f0ef7837f842cb
Administrator <Administrator@10.0.0.200>
2019-03-05 17:13:02 -0500

    message 1

--- src/main/java/flawedPackage/Flawed.java
+++ src/main/java/flawedPackage/Flawed.java
@@ -10,7 +10,7 @@ import java.util.Random;
 @SuppressWarnings("unused")
 public class Flawed {
 
-	// comment to change file ...   
+	// comment to change file ...     
 
 	public static final String PASSWORD = "pass";
 	static byte[] dec;
